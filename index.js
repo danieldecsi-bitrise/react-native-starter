e502ccbd80e2659762e236c7bb1a0d7c61b04904
Dániel Décsi <danieldecsi@users.noreply.github.com>
2022-03-23 17:20:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Register app
 AppRegistry.registerComponent(appName, () => App);
